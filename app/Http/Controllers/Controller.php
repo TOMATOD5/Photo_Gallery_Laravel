bb56936c4f150fbf8b138f451b47c80c881ff0ef
Richard Jonáš <jonas.richard11@gmail.com>
2023-09-14 21:43:14 +0200

    Edit of Contoller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-echo ('Toto je poháněno Frameworkem Laravel');
+echo ('Tato aplikace je poháněna Frameworkem Laravel');
 
 class Controller extends BaseController
 {
